7d26ad160034f2869c1c5be729212a7ee119a3e9
Hari Kammana <hari.kammana@gmail.com>
2023-02-27 15:45:47 +0530

    spring boot project

---
+++ src/main/java/in/javahome/smapp/SmappApplication.java
@@ -0,0 +1,13 @@
+package in.javahome.smapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/javahome/smapp/SmappApplicationTests.java
@@ -0,0 +1,13 @@
+package in.javahome.smapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
